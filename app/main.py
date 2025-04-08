cea0064c272d9867efb20ff59005bf13b401e910
Anupam Siwakoti <anupam.siwakoti@gmail.com>
2025-04-08 01:24:47 -0500

    cqrs enabled

--- app/main.py
+++ app/main.py
@@ -7,15 +7,32 @@ import asyncio
 from app.infrastructure.kafka import consume_messages
 from prometheus_fastapi_instrumentator import Instrumentator
 from app.presentation.websockets import active_connections
-from prometheus_client import Gauge
+from prometheus_client import Gauge, Counter
 from threading import Thread
 import time
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+# Add CORS middleware to allow all methods for your frontend domain
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or specify the domain of your frontend if you want to restrict
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+
 app.include_router(api_router)
 app.include_router(ws_router, prefix="")
 
+# ----------------- Prometheus Metrics -----------------
 ws_connection_gauge = Gauge("websocket_connections_total", "Current active WebSocket connections")
+anomaly_counter = Counter("anomalies_detected_total", "Total number of anomalies detected")
+last_anomaly_value = Gauge("last_anomaly_value", "Value of the last detected anomaly")
+# ------------------------------------------------------
 
 @app.on_event("startup")
 def startup_event():
@@ -25,6 +42,16 @@ def startup_event():
 # to open the consumer just once because i am running the app with --reload
 # and the consumer will be opened multiple times
 consumer_started = False
+
+# Define the callback to update metrics
+def anomaly_callback(data: dict):
+    try:
+        parsed = data if isinstance(data, dict) else json.loads(data)
+        if parsed.get("is_anomaly"):
+            anomaly_counter.inc()
+            last_anomaly_value.set(parsed.get("value", 0.0))
+    except Exception as e:
+        print(f"Callback Error: {e}")
 
 @app.on_event("startup")
 async def start_kafka_consumer():
@@ -38,7 +65,7 @@ Instrumentator().instrument(app).expose(app)
 
 def update_gauge():
     while True:
-        ws_connection_gauge.set(len(active_connections))  # ✅ uses the correct reference
+        ws_connection_gauge.set(len(active_connections))
         time.sleep(10)
 
 @app.on_event("startup")
